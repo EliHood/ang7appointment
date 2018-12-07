e99c46150b16b03a90c35ab39806ef7b7bf3198c
POWER USER <poweruser@Users-iMac-2.local>
2018-12-07 13:06:31 -0500

    MESSAGE

--- src/app/fire-store-date-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FireStoreDatePipePipe } from './fire-store-date-pipe.pipe';
-
-describe('FireStoreDatePipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FireStoreDatePipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/models/schedule.model.ts
+++
@@ -1,7 +0,0 @@
-export class Schedule {
-    id:string;
-    name:string;
-    appointment_date:string;
-    appointment_hour: string;
-   
-}
